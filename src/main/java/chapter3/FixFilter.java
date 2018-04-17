89a4ec6a7379dc32a268ea24943476fb6f54bf41
Senio Caires <seniocaires@gmail.com>
2018-04-17 16:43:25 -0300

    Persons Between 30 and 40 Years Old

--- src/main/java/chapter3/FixFilter.java
+++ src/main/java/chapter3/FixFilter.java
@@ -15,12 +15,28 @@ public class FixFilter {
 		for (Person person : getPersonsLessThan20Years(Persons.getInstance().getPersons())) {
 			System.out.println(person.getAge() + " \t" + person.getGivenName() + " " + person.getSurname());
 		}
+		
+		System.out.println("Persons Between 30 and 40 Years Old");
+		System.out.println("Age \t Name");
+		for (Person person : getPersonsByAgeRange(Persons.getInstance().getPersons(), 30, 40)) {
+			System.out.println(person.getAge() + " \t" + person.getGivenName() + " " + person.getSurname());
+		}
 	}
 
 	public static List<Person> getPersonsLessThan20Years(List<Person> persons) {
 		List<Person> result = new ArrayList<>();
 		for (Person person : persons) {
 			if (person.getAge() < 20) {
+				result.add(person);
+			}
+		}
+		return result;
+	}
+
+	public static List<Person> getPersonsByAgeRange(List<Person> persons, int from, int to) {
+		List<Person> result = new ArrayList<>();
+		for (Person person : persons) {
+			if (person.getAge() >= from && person.getAge() <= to) {
 				result.add(person);
 			}
 		}
