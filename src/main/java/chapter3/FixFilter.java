f699f3c0b3f20a482e40e6f8a77d1546a7759b9a
Senio Caires <seniocaires@gmail.com>
2018-04-17 16:40:28 -0300

    Persons Less Than 20 Years

--- src/main/java/chapter3/FixFilter.java
+++ src/main/java/chapter3/FixFilter.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import data.Person;
+import data.Persons;
 
 public class FixFilter {
 
 	public static void main(String[] args) {
 
-		getPersonsLessThan20Years(new ArrayList<Person>());
+		System.out.println("Persons Less Than 20 Years");
+		System.out.println("Age \t Name");
+		for (Person person : getPersonsLessThan20Years(Persons.getInstance().getPersons())) {
+			System.out.println(person.getAge() + " \t" + person.getGivenName() + " " + person.getSurname());
+		}
 	}
 
 	public static List<Person> getPersonsLessThan20Years(List<Person> persons) {
